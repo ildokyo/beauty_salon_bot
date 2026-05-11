31c96607aea4cb5dfa2b41efdb589a10d4601425
ildokyo <dasha17550@gmail.com>
2026-05-11 14:05:35 +0400

    проверка напоминаний на 14.10

--- scheduler.py
+++ scheduler.py
@@ -57,13 +57,13 @@ def setup_scheduler():
         
         scheduler.add_job(
             send_daily_reminders,
-            trigger=CronTrigger(hour=22, minute=40),
+            trigger=CronTrigger(hour=14, minute=10),
             id="daily_reminders",
             replace_existing=True
         )
         
         scheduler.start()
-        logger.info("🕐 Планировщик запущен. Напоминания в 22:40")
+        logger.info("🕐 Планировщик запущен. Напоминания в 14:10")
         return scheduler
     except Exception as e:
         logger.error(f"❌ Ошибка запуска планировщика: {e}")
